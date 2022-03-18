cd85e6b1a9f92a28805293d0e4b9335d22f91f19
Lily Nguyen <lilynguyen@Lilys-MacBook-Pro.local>
2022-03-17 22:21:12 -0400

    theme social share

--- src/components/IntroSlide.js
+++ src/components/IntroSlide.js
@@ -4,12 +4,12 @@ import SocialMediaShare from "./SocialMediaShare";
 const IntroSlide = () => {
   return (
     <div className="container dark" id="slide-1">
+      <SocialMediaShare />
       <header>
         <h1>&quot;1988&quot;: A Vietnamese Boat Refugee Story</h1>
         <p className="byline">
           By <a href="http://www.lilyn.tech">Lily Nguyen</a>
         </p>
-        <SocialMediaShare />
         <span className="scroll-icon">Scroll</span>
       </header>
     </div>
--- src/components/SocialMediaShare.js
+++ src/components/SocialMediaShare.js
@@ -12,28 +12,16 @@ const SocialMediaShare = () => {
   const shareUrl = "http://github.com";
   const title = "GitHub";
   return (
-    <div className="social-media">
+    <div className="social-share">
       <h2>Share: </h2>
       <div className="share-icons">
-        <EmailShareButton
-          url={shareUrl}
-          quote={title}
-          className="Demo__some-network__share-button"
-        >
+        <EmailShareButton url={shareUrl} quote={title} className="share-btn">
           <EmailIcon size={32} round />
         </EmailShareButton>
-        <FacebookShareButton
-          url={shareUrl}
-          quote={title}
-          className="Demo__some-network__share-button"
-        >
+        <FacebookShareButton url={shareUrl} quote={title} className="share-btn">
           <FacebookIcon size={32} round />
         </FacebookShareButton>
-        <TwitterShareButton
-          url={shareUrl}
-          quote={title}
-          className="Demo__some-network__share-button"
-        >
+        <TwitterShareButton url={shareUrl} quote={title} className="share-btn">
           <TwitterIcon size={32} round />
         </TwitterShareButton>
       </div>
