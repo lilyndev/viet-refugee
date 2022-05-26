0bd1b0c9bf196931d43887e855dfd24fd0276282
Lily N <lilyxdk@gmail.com>
2022-05-26 12:09:42 +0200

    add scroll icon

--- src/components/IntroSlide.js
+++ src/components/IntroSlide.js
@@ -12,7 +12,29 @@ const IntroSlide = () => {
           </p>
           <div className="byline">By Lily Nguyen</div>
         </div>
-        <span className="scroll-icon">Scroll</span>
+        <div className="scroll-icon">
+          <span>Scroll</span>
+          <svg
+            id="scroll-down"
+            width="16"
+            height="34"
+            viewBox="0 0 16 34"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g fill="none">
+              <path
+                id="scroll-down-line"
+                d="M7.99999 34L15.7942 24.25H0.205765L7.99999 34Z"
+                fill="#ffffff"
+              />
+              <path
+                id="scroll-down-arrow"
+                d="M6.99999 0H8.99999V31H6.99999V0Z"
+                fill="#ffffff"
+              />
+            </g>
+          </svg>
+        </div>
       </header>
     </div>
   );
