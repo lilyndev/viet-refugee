610dc5620a56ff967856d1c8baf156f0f054e9ff
Lily N <lilyxdk@gmail.com>
2022-05-26 11:27:15 +0200

    update header font styles

--- src/components/IntroSlide.js
+++ src/components/IntroSlide.js
@@ -2,10 +2,16 @@ import React from "react";
 
 const IntroSlide = () => {
   return (
-    <div className="container xtra-dark" id="slide-1">
+    <div className="slide-intro container xtra-dark" id="slide-1">
       <header>
-        <h1>&quot;1988&quot;: A Vietnamese Boat Refugee Story</h1>
-        <p className="byline">By Lily Nguyen</p>
+        <h1>Viet Refugee</h1>
+        <div className="slide-intro__info">
+          <p>
+            A boat refugee story of how my father fled Vietnam in 1988 after his
+            fifth try.
+          </p>
+          <div className="byline">By Lily Nguyen</div>
+        </div>
         <span className="scroll-icon">Scroll</span>
       </header>
     </div>
