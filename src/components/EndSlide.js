a8a228e1e778d27c24a06e14818168e94a9fbfd6
Lily N <lilyxdk@gmail.com>
2022-06-27 11:56:09 -0400

    update end slide content

--- src/components/EndSlide.js
+++ src/components/EndSlide.js
@@ -12,9 +12,9 @@ const EndSlide = () => {
         <h2>After the Rescue</h2>
         <p>
           After the rescue, Van An Nguyen worked at a refugee camp in Hong Kong.
-          After that, An was sent to Denmark because his sister already lived
-          there. Two years later, visas were approved for his wife and daughter
-          to move to Denmark, and in 1994 they became a family of six.
+          An was then sent to Denmark because his sister lived there. Two years
+          later, in 1990, he could finally reunite with his wife and daughter,
+          and in 1994, they became a family of six.
         </p>
         <h3>Looking for Long-Lost Friend</h3>
         <p>
@@ -22,9 +22,9 @@ const EndSlide = () => {
           amongst the boat people. But An lost contact with Dung in the 90s and
           hopes that sharing his story today will help him reconnect with his
           long-lost friend. The last An heard from Dung was that he had moved to
-          Texas, USA, and below are photos that Dung had sent to An. Please
-          contact the Nguyen family via email or share this story if you have
-          any information that might be helpful.
+          Texas, USA, and below are photos that Dung had sent to An. If you have
+          any information that might be helpful, please contact the Nguyen
+          family via email or share this story.
         </p>
         <figure className="media">
           <img src={imgDung1} alt="" />
@@ -32,12 +32,14 @@ const EndSlide = () => {
           <img src={imgDung3} alt="" />
           <img src={imgDung4} alt="" />
           <figcaption>
-            Photos of Dung Ly with hand-written notes on the back-side of each
-            photo from 1988 and 1989.
+            Photos of Dung Ly with hand-written notes from 1988 and 1989.
           </figcaption>
         </figure>
       </div>
       <ContactUs />
+      <div className="copyright">
+        <p>© 2022 VietRefugee.com</p>
+      </div>
     </div>
   );
 };
