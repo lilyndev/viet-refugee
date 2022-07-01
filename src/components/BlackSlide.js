7d09aafa28723a29effcede908467eaf87e1189c
Lily N <lilyxdk@gmail.com>
2022-07-01 13:40:55 -0400

    update dependency; add proptypes

--- src/components/BlackSlide.js
+++ src/components/BlackSlide.js
@@ -1,9 +1,16 @@
+import PropTypes from "prop-types";
+
 const BlackSLide = (props) => {
   return (
     <div className="container slide-black" id={props.slideId}>
       <div className="content">{props.children}</div>
     </div>
   );
+};
+
+BlackSLide.PropTypes = {
+  slideId: PropTypes.string,
+  children: PropTypes.element.isRequired,
 };
 
 export default BlackSLide;
